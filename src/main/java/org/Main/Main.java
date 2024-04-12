4b0af12928eedb49e03ee04b81ee4e738db7aa76
GRIFFITHH <glqglq032@gmail.com>
2024-04-12 16:35:35 +0900

    java project - gradle 설정 추가 및 CRUD 구현 , 이전에 구현했던 서블릿 코드 삭제

--- src/main/java/org/Main/Main.java
+++ src/main/java/org/Main/Main.java
@@ -1,5 +1,5 @@
 package org.Main;
-import org.Join.UserServlet; // UserServlet 클래스를 임포트합니다.
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
@@ -7,7 +7,7 @@ import org.Join.UserServlet; // UserServlet 클래스를 임포트합니다.
 
 public class Main {
     public static void main(String[] args) {
-        UserServlet userServlet = new UserServlet();
+
 
     }
 }
