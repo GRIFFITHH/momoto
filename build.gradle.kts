d89f787962e0a91371b1eccce3c8aab7e384f18d
GRIFFITHH <glqglq032@gmail.com>
2024-04-12 10:24:46 +0900

    java project - 서블릿 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("war") // 웹 애플리케이션을 위해 WAR 플러그인을 적용
 }
 
 group = "org.example"
@@ -12,6 +13,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // 기존의 테스트 종속성
+
+    // 서블릿 API 종속성 추가
+    implementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
+    // 다른 필요한 종속성들도 여기에 추가합니다.
 }
 
 tasks.test {
